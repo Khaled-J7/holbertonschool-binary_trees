16c063191523ac4ea70a846779ea65f100c907ff
rayen288700 <rayenchuk@gmail.com>
2023-09-22 21:28:50 +0100

    update the code

--- binary_trees.h
+++ binary_trees.h
@@ -15,9 +15,9 @@ struct binary_tree_s
 };
 typedef struct binary_tree_s binary_tree_t;
 
-
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 void binary_tree_print(const binary_tree_t *);
-
+int compare_depth(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -27,4 +27,12 @@ int binary_tree_is_root(const binary_tree_t *node);
 size_t binary_tree_height(const binary_tree_t *tree); 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
+size_t binary_tree_height_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+binary_tree_t *search_uncle(binary_tree_t *node);
+binary_tree_t * binary_tree_uncle(binary_tree_t * node);
+
 #endif
